ca3c4c6d2203d46c96d1d9dd4a3ff7496f208331
Sergio <sergiopdjr@hotmail.com>
2025-01-03 14:51:49 -0300

    components setup and final resolution

---
+++ Arithmetic_Calculator/src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <a href="https://github.com/DiasSergio" target="_blank"><img src="./icons/github.svg"
+                        alt="github logo"></a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/sergio-pereira-dias-junior-790557161/" target="_blank"><img
+                        src="./icons/linkedin.svg" alt="linkedin logo"></a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+ul {
+    display: flex;
+    justify-content: center;
+    padding-left: 0;
+}
+
+li {
+    list-style: none;
+}
+
+a {
+    padding: 10px;
+    text-decoration: none;
+}
+
+img {
+    max-width: 50px;
+    width: 100%;
+}
+</style>
---
+++ Arithmetic_Calculator/src/components/Results.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps(['doTheMath'])
+</script>
+
+<template>
+    <div class="alert alert-secondary finalResult col-md-6" role="alert">
+        {{ props.doTheMath }}
+    </div>
+</template>
+
+<style scoped>
+.finalResult {
+    display: flex;
+    justify-content: center;
+    font-weight: bold;
+    margin-top: 5px;
+    font-size: large;
+    margin: 15px auto;
+}
+</style>
